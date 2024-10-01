7835985e628bf23c1f5e489a0995c99bce8d9dff
ASUS <najouintissar@gmail.com>
2024-10-01 19:31:26 +0200

    Added rest java code

---
+++ src/main/java/de/bs1ts/It12a/bauer/noteservice/NoteController.java
@@ -0,0 +1,22 @@
+package de.bs1ts.It12a.bauer.noteservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v3/notes")
+public class NoteController {
+
+@GetMapping
+
+Note[] getAll() {
+    Note[] data = {
+            new Note("HTTP lernen"),
+            new Note("Rest-Prinzipien lernen"),
+            new Note("Zimmer aufräumen"),
+            new Note("Staub saugen")
+    };
+    return  data;
+}
+}
---
+++ src/main/java/de/bs1ts/It12a/bauer/noteservice/NoteserviceApplication.java
@@ -0,0 +1,14 @@
+package de.bs1ts.It12a.bauer.noteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteserviceApplication.class, args);
+
+	}
+
+}
