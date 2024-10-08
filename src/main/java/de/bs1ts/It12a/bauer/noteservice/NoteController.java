edf9ed7de386a564c88f2e8220c0e2af123784b1
MasterChief177 <kristof.flajsz@outlook.com>
2024-10-08 20:32:54 +0200

    added Notes

--- src/main/java/de/bs1ts/It12a/bauer/noteservice/NoteController.java
+++ src/main/java/de/bs1ts/It12a/bauer/noteservice/NoteController.java
@@ -1,6 +1,8 @@
 package de.bs1ts.It12a.bauer.noteservice;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
